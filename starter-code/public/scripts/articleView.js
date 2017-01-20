0c4ff7c0e2da7105505e6b5e2fd58289c457b01e
Alejandro Garcia <agash2013@outlook.com>
2017-01-19 17:43:11 -0800

    added some changes

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -112,11 +112,13 @@
 
   articleView.initAdminPage = function() {
     // DONE: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
-    var template = Handlebars.compile($('#article-template').text());
+    var template = Handlebars.compile($('#author-template').text());
+
     // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
     // appending to the DOM.
     // The callback is not required to return anything.
-    Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
+    Article.numWordsByAuthor().forEach(stat =>
+      $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
     $('#blog-stats .articles').text(Article.all.length);
